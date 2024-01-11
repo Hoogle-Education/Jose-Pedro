9211ecf5091ea01051552b3489a6c7b72f28a00a
hgrafa <hugorafell@gmail.com>
2024-01-11 10:38:52 -0300

    initial commit

---
+++ ReviewPOO/ReviewPOO/Model/Funcionario.cs
@@ -0,0 +1,57 @@
+﻿namespace ReviewPOO.Model;
+
+public class Funcionario
+{
+    // tem - atributos
+    private int _id;
+    private string _nome;
+    private double _salario;
+    private double _imposto;
+    private string _cargo;
+
+    // (metodo) construtor
+    public Funcionario() {
+    }
+
+    public Funcionario(int id, string nome, double salario)
+    {
+        _id = id;
+        _nome = nome;
+        _salario = salario;
+        _imposto = salario * 0.1;
+    }
+
+    public Funcionario(int id, string nome, double salario, string cargo)
+        : this(id, nome, salario)
+    {
+        _cargo = cargo;
+    }
+
+    public string Nome {
+        get {  return _nome; }
+        set {
+            if (value != null)
+            {
+                _nome = value;
+            }
+        }
+    }
+
+    public double SalarioLiquido
+    {
+        get { return _salario - _imposto;  }
+    }
+
+    // faz - metodos
+    public void ReceberAumento(double aumento)
+    {
+        _salario += aumento;
+    }
+
+    public void ReceberAumento(double aumento, double taxaPerformance)
+    {
+        _salario += aumento;
+        double porcentagemAumento = 1 + taxaPerformance;
+        _salario *= porcentagemAumento;
+    }
+}
---
+++ ReviewPOO/ReviewPOO/Program.cs
@@ -0,0 +1,51 @@
+﻿using ReviewPOO.Model;
+
+namespace ReviewPOO; 
+
+public class Program
+{
+    //public static void ApresentarFuncionario(int index, string[] ids, string[] nomes)
+    //{
+    //    Console.WriteLine(ids[index]);
+    //    Console.WriteLine(nomes[index]);
+    //}
+
+    //int quantidade = 1000;
+
+    //string[] ids = new string[quantidade];
+    //string[] nomes = new string[quantidade];
+    //double[] salarios = new double[quantidade];
+    //string[] cargos = new string[quantidade];
+
+    //int a = 2, b = 3; // a = 2, b = 3
+    //int aux = a; // a = 2, b = 3, aux = 2
+    //a = b; // a = 3, b = 3, aux = 2
+    //    b = aux; // a = 3, b = 2, aux = 2
+
+    //ApresentarFuncionario(3, ids, nomes);
+
+    public static void Main() // entrypoint
+    {
+        Funcionario f1 = new Funcionario(1, "Joao da Silva", 2000);
+        Funcionario f2 = new Funcionario(2, "Maria da Silva", 5000);
+
+        //f1.nome = "Joao da Silva";
+        //f2.nome = "Maria da Silva";
+        // f1.salario = -200;
+
+        Funcionario aux = f1;
+        f1 = f2;
+        f2 = aux;
+
+        Funcionario[] funcionarios = new Funcionario[1000];
+
+        funcionarios[0] = new Funcionario();
+        funcionarios[1] = f1;
+
+        f1.Nome = "teste";
+
+        Console.WriteLine( funcionarios.Length );
+        Console.WriteLine(f1.SalarioLiquido);
+
+    }
+}
